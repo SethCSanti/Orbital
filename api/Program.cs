9d23cfdd0bc22c33c17273805f399c4da62191b5
Seth Conner <sconner1@byu.edu>
2026-07-14 15:27:17 -0600

    routed controllers and OpenAPI/Swagger in program.cs middleware

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Orbital API");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+// Endpoint mapping phase
+app.MapControllers();
 
 app.Run();
