741e44bc1a6095e1879f12be3d31246402d80c10
Seth Conner <sconner1@byu.edu>
2026-07-16 14:52:01 -0600

    feat(api): add CacheKeys constants for Redis cache keys

---
+++ api/infrastructure/CacheKeys.cs
@@ -0,0 +1,13 @@
+namespace Orbital.Api.Infrastructure;
+
+public static class CacheKeys
+{
+    public const string IssPosition = "iss:position";
+    public const string UpcomingLaunches = "launches:upcoming";
+    public const string Astronauts = "astronauts:current";
+    public const string MissionHistory = "missions:history";
+    public const string Apod = "apod:today";
+    public const string AsteroidFeed = "asteroids:feed";
+    public const string RocketData = "rockets:data";
+    public const string ExoplanetData = "exoplanets:data";
+}
